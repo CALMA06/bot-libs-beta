0dc1f861a8d73c586b58b3994d22a50c3c85b37a
Jonathan Ford <jonathanwford156@gmail.com>
2019-02-27 10:31:31 -0500

    add main file

---
+++ bot.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+const { AkairoClient, CommandHandler, ListenerHandler } = require('discord-akairo');
+
+class BotClient extends AkairoClient {
+    constructor() {
+        super({
+            ownerID: ['273707798670344192', '102561906765619200', '273849600664797185']
+        });
+
+        this.commandHandler = new CommandHandler(this, {
+            automateCategories: true,
+            commandUtil: true,
+            defaultPrompt: {
+                retries: 5,
+                cancel: msg => `${msg.author}, this command has been canceled.`,
+                ended: msg => `${msg.author}, the max amount of retries has been reached.`,
+                timeout: msg => `${msg.author}, time is up on this command.`
+            },
+            directory: './commands',
+            handleEdits: true,
+            prefix: 'f;'
+        });
+
+        this.listenerHandler = new ListenerHandler(this, {
+            automateCategories: true,
+            directory: './listeners'
+        });
+
+        this.commandHandler.loadAll();
+        this.listenerHandler.loadAll();
+    }
+}
+
+const bot = new BotClient();
+
+bot.login(process.env.TOKEN);
+
+process.on('unhandledRejection', console.error);
