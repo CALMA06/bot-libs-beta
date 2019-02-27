11ea1b4118a5dc4452e99722b21a1bb66f2bdb5c
Jonathan Ford <jonathanwford156@gmail.com>
2019-02-27 10:34:21 -0500

    add ping command and ready listener

---
+++ commands/util/ping.js
@@ -0,0 +1,19 @@
+const { Command } = require('discord-akairo');
+const { oneLine } = require('common-tags');
+
+module.exports = class PingCommand extends Command {
+    constructor() {
+        super('ping', {
+            aliases: ['ping'],
+            category: 'util',
+            description: oneLine`This command measures (in milliseconds) how long it takes the bot to receive your
+            message and respond back to it.`
+        });
+    }
+
+    async exec(msg) {
+        const pingMsg = await msg.channel.send('Hello?');
+    
+        return pingMsg.edit(`${msg.member.user.tag}, your ping is ${pingMsg.createdTimestamp - msg.createdTimestamp}ms.`);
+    }
+};
